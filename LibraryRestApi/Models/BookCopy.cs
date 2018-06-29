c45f2dd2832486ccaaac59ff8cab1080e34b5dc8
Piotr Kadziewicz <piotr.kadziewicz@outlook.com>
2018-06-29 08:01:50 +0200

    Starting project

---
+++ LibraryRestApi/Models/BookCopy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryRestApi.Models
+{
+    public class BookCopy
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime SignUpDate { get; set; }
+
+        public ISet<BookRental> bookRentals { get; set; }
+    }
+}
